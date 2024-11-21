172f831192b63b33ed99332ff360896d7bdd0a08
Dux27 <filip.jaworski279@gmail.com>
2024-11-21 23:36:17 +0100

    test

--- calculator.py
+++ calculator.py
@@ -5,8 +5,10 @@ class MainApplication(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
 
+    
 
 if __name__ == "__main__":
     root = tk.Tk()
     MainApplication(root).pack(side="top", fill="both", expand=True)
     root.mainloop()
+    print("test")
